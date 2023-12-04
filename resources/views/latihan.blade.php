9a2f5dcf8f8c27172aa0371c7cd398a7e30daa26
arief2020 <msyaifullahalarief@gmail.com>
2023-12-04 20:15:26 +0700

    first commit not final

---
+++ resources/views/latihan.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing Page Qword</title>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="{{ asset('asset/css/mystyle.css') }}">
+</head>
+<body>
+    <div class="discount-header">Ada Promo Hingga 50% <a href="#">Cek Sekarang</a></div>
+    <div class="top-nav">
+        <a href="#"><img alt="" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp"></a>
+        <a href="#home">Cloud Hosting</a>
+        <a href="#news">Server</a>
+        <a href="#contact">Domain</a>
+        <a href="#about">Email Suite</a>
+        <a href="#about">Service</a>
+        <a href="#about">Internet Access</a>
+        <button class="button-login">Login</button>
+      </div>
+    <div class="hero">
+        <h1>Cloud Hosting untuk<span> Kesuksesan Websitemu!</span></h1>
+        <p>
+            Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.
+        </p>
+        <h3>#QwordsYourSuccess</h3>
+        <button class="btn bg-qw-primary">pesan sekarang</button>
+    </div>
+    <button class="btn btn-primary">halo</button>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> --}}
+</body>
+</html>
